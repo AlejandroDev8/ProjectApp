742f6671ff51d23a3347b88611cd8da7e5e31fac
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-20 03:16:34 -0600

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/includes/admin/navigation.blade.php
+++ resources/views/layouts/includes/admin/navigation.blade.php
@@ -98,14 +98,6 @@
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
-                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                {{ __('API Tokens') }}
-                            </x-dropdown-link>
-                        @endif
-
-                        <div class="border-t border-gray-200"></div>
-
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}" x-data>
                             @csrf
