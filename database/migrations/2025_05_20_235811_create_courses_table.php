c4e5f3a828d41dbc82c8b445db0be8b458a78b9b
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-20 18:37:38 -0600

    Add name column to levels and categories tables; add multiple fields to courses table with foreign key constraints

--- database/migrations/2025_05_20_235811_create_courses_table.php
+++ database/migrations/2025_05_20_235811_create_courses_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('level_id')->constrained()->onDelete('cascade');
+            $table->foreignId('price_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('summary')->nullable();
+            $table->text('description');
+            $table->integer('status')->default(1);
+            $table->string('image_path')->nullable();
+            $table->string('video_path')->nullable();
+            $table->text('welcome_message')->nullable();
+            $table->text('goodbye_message')->nullable();
+            $table->text('observations')->nullable();
             $table->timestamps();
         });
     }
