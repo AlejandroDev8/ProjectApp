0b23e467e18bf9fcd9f2e724593f23bb59425125
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-20 19:39:07 -0600

    Add fillable properties to Category, Course, Level, and Price models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'level_id',
+        'price_id',
+        'name',
+        'slug',
+        'summary',
+        'description',
+        'status',
+        'image_path',
+        'video_path',
+        'welcome_message',
+        'goodbye_message',
+        'observations',
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
+
+    public function price()
+    {
+        return $this->belongsTo(Price::class);
+    }
 }
