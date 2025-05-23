c07699f86db14654685fecc1a37d03e607a2d24a
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-23 17:19:35 -0600

    Add categories, levels, and prices data to course edit view

--- app/Http/Controllers/Instructor/CourseController.php
+++ app/Http/Controllers/Instructor/CourseController.php
@@ -67,7 +67,11 @@ class CourseController extends Controller
      */
     public function edit(Course $course)
     {
-        return view('instructor.courses.edit', compact('course'));
+        $categories = Category::all();
+        $levels = Level::all();
+        $prices = Price::all();
+
+        return view('instructor.courses.edit', compact('course', 'categories', 'levels', 'prices'));
     }
 
     /**
