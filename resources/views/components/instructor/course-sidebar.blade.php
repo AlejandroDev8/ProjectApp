548420697ac921329f27ec394afd0ed12d5df1a8
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-25 14:40:06 -0600

    Refactor course sidebar to use dynamic links array for improved maintainability and readability

--- resources/views/components/instructor/course-sidebar.blade.php
+++ resources/views/components/instructor/course-sidebar.blade.php
@@ -1,24 +1,38 @@
 @props(['course'])
 
+@php
+    $links = [
+        [
+            'name' => 'Course Information',
+            'url' => route('instructor.courses.edit', $course),
+            'icon' => 'fa-solid fa-circle-info',
+            'active' => request()->routeIs('instructor.courses.edit'),
+        ],
+        [
+            'name' => 'Promotional Video',
+            'url' => route('instructor.courses.video', $course),
+            'icon' => 'fa-solid fa-video',
+            'active' => request()->routeIs('instructor.courses.video'),
+        ],
+    ];
+@endphp
+
 <div class="grid lg:grid-cols-5 grid-cols-1 gap-6">
     <aside class="col-span-1">
         <h1 class="font-semibold text-xl mb-4">
             Course Edition
         </h1>
         <nav>
-            <ul>
-                <li class="border-l-4 border-indigo-400 pl-3">
-                    <a href="{{ route('instructor.courses.edit', $course) }}"
-                        class="text-gray-600 hover:text-gray-900 font-semibold">
-                        Course Information
-                    </a>
-                </li>
-                <li class="border-l-4 border-transparent pl-3">
-                    <a href="{{ route('instructor.courses.video', $course) }}"
-                        class="text-gray-600 hover:text-gray-900 font-semibold">
-                        Promotional Video
-                    </a>
-                </li>
+            <ul class="space-y-2">
+                @foreach ($links as $link)
+                    <li class="border-l-4 {{ $link['active'] ? 'border-indigo-400' : 'border-transparent' }} pl-3">
+                        @if (isset($link['icon']))
+                            <i class="{{ $link['icon'] }} mr-1 text-indigo-600"></i>
+                        @endif
+                        <a href="{{ $link['url'] }}"
+                            class="text-gray-600 hover:text-gray-900 font-semibold">{{ $link['name'] }}</a>
+                    </li>
+                @endforeach
             </ul>
         </nav>
     </aside>
