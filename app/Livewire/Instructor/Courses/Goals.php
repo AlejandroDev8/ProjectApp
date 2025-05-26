4df49fb725504eb5d2b5aa49227d974e98253385
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-26 01:59:08 -0600

    Add Livewire component and Blade view for course goals management

---
+++ app/Livewire/Instructor/Courses/Goals.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Livewire\Instructor\Courses;
+
+use App\Models\Goal;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class Goals extends Component
+{
+    public $course;
+
+    public $goals;
+
+    #[Validate('required|string|max:255')]
+    public $title;
+
+    public function mount($course)
+    {
+        $this->goals = Goal::where('course_id', $course->id)->get()->toArray();
+    }
+
+    public function store()
+    {
+        $this->validate();
+
+        $this->course->goals()->create([
+            'title' => $this->title,
+        ]);
+
+        $this->goals = Goal::where('course_id', $this->course->id)->get()->toArray();
+
+        $this->reset('title');
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'goals.*.title' => 'required|string|max:255',
+        ]);
+
+        foreach ($this->goals as $goal) {
+            Goal::find($goal['id'])->update(['title' => $goal['title']]);
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.instructor.courses.goals');
+    }
+}
