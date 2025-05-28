41d05de0982d95a6800127a893190b87a74a6dfd
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-28 02:53:37 -0600

    Add requirements management view and functionality for courses

--- resources/views/instructor/courses/requirements.blade.php
+++ resources/views/instructor/courses/requirements.blade.php
@@ -5,6 +5,8 @@
         </h2>
     </x-slot>
     <x-container class="py-8">
-        <x-instructor.course-sidebar :course="$course"></x-instructor.course-sidebar>
+        <x-instructor.course-sidebar :course="$course">
+            @livewire('instructor.courses.requirements', ['course' => $course])
+        </x-instructor.course-sidebar>
     </x-container>
 </x-instructor-layout>
---
+++ resources/views/livewire/instructor/courses/requirements.blade.php
@@ -0,0 +1,75 @@
+<div>
+    <ul class="mb-4 space-y-3" id="requirements-list">
+        @foreach ($requirements as $index => $require)
+            <li wire:key="requirement-{{ $require['id'] }}" data-id="{{ $require['id'] }}">
+                <div class="flex">
+                    <x-input wire:model="requirements.{{ $index }}.title" class="flex-1 rounded-r-none" />
+                    <div class="flex items-center border border-l-0 border-gray-300 divide-x divide-gray-300 rounded-r">
+                        <button class="px-2 hover:text-red-600" onclick="destroyRequirement({{ $require['id'] }}) ">
+                            <i class="far fa-trash-alt"></i>
+                        </button>
+                        <div class="px-2 cursor-move">
+                            <i class="fas fa-bars"></i>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    <div class="flex justify-end mb-8">
+        @if (count($requirements) < 0)
+            <x-button wire:click="update">
+                {{ __('Update') }}
+            </x-button>
+        @else
+            <span class="text-gray-500">{{ __('No requirements yet. Please add one.') }}
+            </span>
+        @endif
+    </div>
+    <form wire:submit.prevent="store">
+        <div class="bg-gray-100 rounded-lg shadow-lg p-6">
+            <x-label for="requirements" :value="__('New Requiere')" />
+            <x-input id="requirements" class="w-full mt-4" placeholder="Enter a new requierement" wire:model="title" />
+            <x-input-error for="title" class="mt-2" />
+            <div class="flex justify-end">
+                <x-button class="mt-4">
+                    {{ __('Add Requierement') }}
+                </x-button>
+            </div>
+        </div>
+    </form>
+
+    @push('js')
+        <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.6/Sortable.min.js"></script>
+        <script>
+            const requirementsList = document.getElementById('requirements-list');
+            new Sortable(requirementsList, {
+                animation: 150,
+                ghostClass: 'blue-background-class',
+                store: {
+                    set: (sortable) => {
+                        @this.call('sortRequirements', sortable.toArray())
+                    }
+                }
+            });
+        </script>
+        <script>
+            destroyGoal = (id) => {
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        @this.call('destroyRequirement', id);
+                        Swal.fire("Deleted!", "Your requirement has been deleted.", "success");
+                    }
+                });
+            }
+        </script>
+    @endpush
+</div>
