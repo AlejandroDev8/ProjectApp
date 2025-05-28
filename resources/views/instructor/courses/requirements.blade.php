df6595b89bc2aa92d5373091bc314bdf2cf3a3af
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-28 02:23:12 -0600

    Add requirements view and route to CourseController for course requirements management

---
+++ resources/views/instructor/courses/requirements.blade.php
@@ -0,0 +1,10 @@
+<x-instructor-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Course: ' . $course->title) }}
+        </h2>
+    </x-slot>
+    <x-container class="py-8">
+        <x-instructor.course-sidebar :course="$course"></x-instructor.course-sidebar>
+    </x-container>
+</x-instructor-layout>
