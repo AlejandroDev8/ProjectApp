be9fd2c5ba7b3c74807f88f616a6007754acf9ad
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-28 16:42:07 -0600

    Refactor code structure for improved readability and maintainability

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,79 @@
-<x-app-layout></x-app-layout>
+<x-app-layout>
+    <figure class="mb-20">
+        <img src="{{ asset('img/cover.jpg') }}" alt="Cover Image"
+            class="w-full h-auto md:aspect-[3/1] aspect-square object-cover object-center">
+    </figure>
+    <section>
+        <x-container>
+            <h1 class="text-2xl uppercase font-semibold text-center mb-8">
+                Welcome to the Laravel Starter
+            </h1>
+            <ul class="grid grid-cols-4 gap-6">
+                <li>
+                    <a href="">
+                        <img src="https://codersfree.com/img/servicios/cursos.jpeg" alt="Courses Image"
+                            class="aspect-video object-center obeject-cover rounded-lg">
+                    </a>
+                    <h1 class="text-xl text-center font-semibold mb-2 mt-2">
+                        <a href="">
+                            Courses Online
+                        </a>
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti id corporis porro placeat
+                        labore delectus repudiandae facilis similique. Distinctio quisquam ipsa quos illum ex dicta
+                        rerum saepe expedita fugiat quibusdam?
+                    </p>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="https://codersfree.com/img/servicios/desarrollo.jpeg" alt="Courses Image"
+                            class="aspect-video object-center obeject-cover rounded-lg">
+                    </a>
+                    <h1 class="text-xl text-center font-semibold mb-2 mt-2">
+                        <a href="">
+                            Web Development
+                        </a>
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti id corporis porro placeat
+                        labore delectus repudiandae facilis similique. Distinctio quisquam ipsa quos illum ex dicta
+                        rerum saepe expedita fugiat quibusdam?
+                    </p>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="https://codersfree.com/img/servicios/asesorias.jpg" alt="Courses Image"
+                            class="aspect-video object-center obeject-cover rounded-lg">
+                    </a>
+                    <h1 class="text-xl text-center font-semibold mb-2 mt-2">
+                        <a href="">
+                            Online Advisories
+                        </a>
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti id corporis porro placeat
+                        labore delectus repudiandae facilis similique. Distinctio quisquam ipsa quos illum ex dicta
+                        rerum saepe expedita fugiat quibusdam?
+                    </p>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="https://codersfree.com/img/servicios/blog.jpeg" alt="Courses Image"
+                            class="aspect-video object-center obeject-cover rounded-lg">
+                    </a>
+                    <h1 class="text-xl text-center font-semibold mb-2 mt-2">
+                        <a href="">
+                            Blog
+                        </a>
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti id corporis porro placeat
+                        labore delectus repudiandae facilis similique. Distinctio quisquam ipsa quos illum ex dicta
+                        rerum saepe expedita fugiat quibusdam?
+                    </p>
+                </li>
+            </ul>
+        </x-container>
+    </section>
+</x-app-layout>
