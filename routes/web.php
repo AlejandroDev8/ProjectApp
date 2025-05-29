2c39ddc0260024c76a99b66f5c08797bc3c610e5
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-29 04:41:54 -0600

    Add CheckOutController and route for checkout page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckOutController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -13,5 +14,9 @@ Route::get('courses', [CourseController::class, 'index'])
 
 Route::get('courses/{course}', [CourseController::class, 'show'])
     ->name('courses.show');
+
 Route::get('cart', [CartController::class, 'index'])
     ->name('cart.index');
+
+Route::get('checkout', [CheckOutController::class, 'index'])
+    ->name('checkout.index');
