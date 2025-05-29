9da940f7a25b9b6c8a1680bffcd3ab778032f477
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-05-28 23:55:43 -0600

    Implement WelcomeController and update routing for course display on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('img/cover.jpg') }}" alt="Cover Image"
             class="w-full h-auto md:aspect-[3/1] aspect-square object-cover object-center">
     </figure>
-    <section>
+    <section class="mb-24">
         <x-container>
             <h1 class="text-2xl uppercase font-semibold text-center mb-8">
                 Welcome to the Laravel Starter
@@ -73,6 +73,53 @@
                         rerum saepe expedita fugiat quibusdam?
                     </p>
                 </li>
+            </ul>
+        </x-container>
+    </section>
+    <section>
+        <x-container>
+            <h1 class="text-2xl font-semibold text-center uppercase mb-8">
+                Our Courses
+            </h1>
+            <ul class="grid grid-cols-4 gap-6">
+                @foreach ($courses as $course)
+                    <li>
+                        <div class=" bg-white rounded-lg overflow-hidden">
+                            <figure>
+                                <img src="{{ $course->image }}" alt="{{ $course->title }}"
+                                    class="w-full aspect-video object-cover">
+                            </figure>
+                            <div class="px-6 py-4 pb-5">
+                                <h2 class="line-clamp-2 text-lg leading-5 min-h-[42px]">
+                                    <a href="">
+                                        {{ $course->title }}
+                                    </a>
+                                </h2>
+                                <p class="text-gray-500 text-sm mt-2">
+                                    Level: {{ $course->level->name }}
+                                </p>
+
+                                <p class="font-semibold mt-2">
+                                    @if ($course->price->value == 0)
+                                        <span class="text-green-500 font-semibold text-lg">
+                                            Free
+                                        </span>
+                                    @else
+                                        <span class="text-gray-500 font-semibold text-lg">
+                                            ${{ $course->price->price }} USD
+                                        </span>
+                                    @endif
+                                </p>
+                                <a href="">
+                                    <button
+                                        class="block w-full text-center mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
+                                        View Course
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                @endforeach
             </ul>
         </x-container>
     </section>
